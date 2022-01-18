cdee25daaee4dace6b44c86bd64261c4fc321db4
mizza-cz <mizza.cz666@gmail.com>
2022-01-18 12:02:20 +0100

    add fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,43 +1,38 @@
 
+let el = document.getElementById('tipy-btn');
+if(el){
+  el.addEventListener('click', function(){
+    errorTipy = document.querySelector(".ui.error.message"),
+  successTipy = document.querySelector(".ui.success.message");
 
-
-
-
-
-
- 
+  let inputTipy = document.querySelectorAll('tipovani-item__input-text')
+  if(inputTipy <= 0){
+        errorTipy.className = "ui error message ";
+     
+     } else{
+        successTipy.className = "ui success message";
+        
+     }
+  });
+}
 
 jQuery(document).ready(function($) {
   $('.tipovani__inner').slick({
-    prevArrow: '<div class="slick-current"><button class="tipovani__btn tipovani__btnprev"><img src="images/svg/angle-down.svg" alt="" ></button></div> ',
+    prevArrow: '<button class="tipovani__btn tipovani__btnprev"><img src="images/svg/angle-down.svg" alt="" ></button> ',
     nextArrow: ' <button class="tipovani__btn tipovani__btnnext"><img src="images/svg/angle-down.svg" alt = "" ></button > ',
     // centerMode: true,
-    slidesToShow: 5,
+    variableWidth: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     infinite:false,
     responsive: [
       
-     {
-       breakpoint: 1441,
-       settings: {
-         slidesToShow:4,
-       }
-     },
-     {
-      breakpoint: 1241,
-      settings: {
-        slidesToShow:3,
-      }
-    },
+   
     {
-      breakpoint: 921,
-      settings: {
-        slidesToShow:2,
-      }
-    },
-    {
-      breakpoint: 641,
+      breakpoint: 541,
       settings: {
         slidesToShow:1,
+        centerMode: true,
         
       }
     },
@@ -72,12 +67,82 @@ jQuery(document).ready(function($) {
   });
 
  
- });    
+ }); 
 
+function myFunction() {
+ var x = document.getElementById("myInput");
+ if (x.type === "password") {
+   x.type = "text";
+ } else {
+   x.type = "password";
+ }
+}
 
+  $(document).ready(function(){
+    function updateText(event){
+      var input=$(this);
+      setTimeout(function(){
+        var val=input.val();
+        if(val!="")
+          input.parent().addClass("label-active");
+        else
+          input.parent().removeClass("label-active");
+      },1)
+    }
+    $(".tipovani-item__input input").keydown(updateText);
+    $(".tipovani-item__input input").change(updateText);
+  });
+  function limitKeypress(event, value, maxLength) {
+    if (value != undefined && value.toString().length >= maxLength) {
+        event.preventDefault();
+    }
+    return
+  }
 
+ var validations = {
+  required: function(value){
+    return value !== '';
+  },
+  phone: function(value){
+    return value.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+);
+  },
+  email: function(value){
+    return value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  }
+}
+let valed = function validate() {
+  var form = document.getElementById('form'),
+      inputsArr = form.querySelectorAll('input'),
+      errorMessage = document.querySelector(".ui.error.message"),
+      successMessage = document.querySelector(".ui.success.message");
+  
+  form.addEventListener('submit', function(e){
+    var i = 0;
+    while (i < inputsArr.length ) {
+      var attr = inputsArr[i].getAttribute('data-validation'),
+          rules = attr ? attr.split(' ') : '',
+          parent = inputsArr[i].closest(".field"),
+          j = 0;
+      while (j < rules.length) {
+        if(!validations[rules[j]](inputsArr[i].value)) {
+          e.preventDefault();
+          errorMessage.className = "ui error message";
+          errorMessage.innerHTML = "musí být vyplněno!";
+          parent.className = "field error";
+          return false;
+        }
+        errorMessage.className = "ui error message hidden";
+        parent.className = "field";
+        j++;
+      }
+      i++;
+    }
+    e.preventDefault();
+    successMessage.className = "ui success message";
+    // form.outerHTML = "";
+    // delete form;
+  }, false)
+validate();
 
-
- 
-
- 
+}
