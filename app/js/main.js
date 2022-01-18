87a101de135d692b382060d72d58c27b529702e4
mizza-cz <mizza.cz666@gmail.com>
2022-01-18 13:23:34 +0100

    add fix

--- app/js/main.js
+++ app/js/main.js
@@ -98,6 +98,16 @@ function myFunction() {
     }
     return
   }
+  // Prevent NULL input and replace text.
+$(document).on('change', 'input[type="number"]', function (event) {
+  this.value = this.value.replace(/[^0-9]+/g, '');
+  if (this.value < 1) this.value = 0;
+});
+
+// Block non-numeric chars.
+$(document).on('keypress', 'input[type="number"]', function (event) {
+  return (((event.which > 47) && (event.which < 58)) || (event.which == 13));
+});
 
  var validations = {
   required: function(value){
